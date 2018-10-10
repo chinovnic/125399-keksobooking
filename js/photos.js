7e289aa6cb49a59a3e12de42dd499d0b616266d4
daria <daria@garpix.com>
2018-10-10 14:40:48 +0300

    Начинает чинить функцию множественной загрузки

--- js/photos.js
+++ js/photos.js
@@ -24,32 +24,62 @@
     }
   };
 
+  function clearPhotos() {
+    var photos = document.querySelectorAll('.ad-form__photo');
+    photos.forEach(function(item) {
+      item.remove();
+    });
+  }
+
   var loadSomePhotos = function (fileChooser, preview, parentBlock) {
     var filesArray = fileChooser.files;
-    var reader = new FileReader();
-    var photoPreviewClone = preview.cloneNode(true);
-    for (var i = 0; i < filesArray.length; i++) {
-      var fileName = filesArray[i].name.toLowerCase();
-      var matches = FILE_TYPES.some(function (it) {
-        return fileName.endsWith(it);
+    clearPhotos();
+    for(item in filesArray) {
+      console.log(item);
+      var reader = new FileReader();
+      reader.addEventListener('load', function () {
+        var photoPreviewClone = preview.cloneNode(true);
+        var img = photoPreviewClone.querySelector('img')
+        var newImage = document.createElement('img');
+        photoPreviewClone.appendChild(newImage);
+        parentBlock.appendChild(photoPreviewClone);
       });
-      if (matches) {
-        reader.addEventListener('load', function (evt) {
-          //судя по всему, я не обращаюсь к тому блоку, который вставляю
-          photoPreviewClone.backgroundImage = 'url(' + evt.target.result + ')';
-          parentBlock.appendChild(photoPreviewClone);
-        });
-        if (i > 0) {
-        //   var photoPreviewAll = document.querySelectorAll('.ad-form__photo');
-        //   photoPreviewAll[0].classList.add('visually-hidden');
-        }
-        reader.readAsDataURL(filesArray[i]);
-        //Ошибку выдает, когда пытаюсь загрузить сразу несколько файлов. В этом месте он видит их сразу все, а не только отекущий
-        console.log(filesArray[i]);
-      }
+      reader.readAsDataURL(file);
+
     }
-    photoPreviewBlock.classList.add('visually-hidden');
+    console.log(filesArray, 'filesArray');
+
+    // for (var i = 0; i < filesArray.length; i++) {
+    //   var fileName = filesArray[i].name.toLowerCase();
+    //   var matches = FILE_TYPES.some(function (it) {
+    //     return fileName.endsWith(it);
+    //   });
+    //   if (matches) {
+    //     reader.addEventListener('load', function (evt) {
+    //       var photoPreviewClone = preview.cloneNode(true);
+
+    //       parentBlock.appendChild(photoPreviewClone);
+    //     });
+    //     if (i > 0) {
+    //     //   var photoPreviewAll = document.querySelectorAll('.ad-form__photo');
+    //     //   photoPreviewAll[0].classList.add('visually-hidden');
+    //     }
+    //     reader.readAsDataURL(filesArray[i]);
+
+    //     console.log(filesArray[i]);
+    //   }
+    // }
+    // photoPreviewBlock.classList.add('visually-hidden');
   };
+
+  var createImage = function () {
+    var newImage = document.createElement('img');
+    photoPreviewBlock.appendChild(newImage);
+    newImage.setAttribute('src', 'img/muffin-grey.svg');
+    newImage.setAttribute('width', '70');
+    newImage.setAttribute('height', '70');
+  };
+  createImage();
 
   var onAvatarChange = function () {
     loadPhoto(avatarInput, avatarPreview);
